b6ff78033671b96e0d2988b57fd6e047cc21c817
kuba jarosz <kuba.jarooosz@gmail.com>
2022-02-13 19:28:35 +0100

    classes Board, Ship and Battleship completed

--- src/main/java/com/kodilla/battleship/Ship.java
+++ src/main/java/com/kodilla/battleship/Ship.java
@@ -8,17 +8,26 @@ public class Ship extends Parent {
 
     private int health;
 
-    public Ship(int type, boolean vertical){
+    public Ship(int type, boolean vertical) {
         this.type = type;
         this.vertical = vertical;
         health = type;
+
+        /*VBox vbox = new VBox();
+        for (int i = 0; i < type; i++) {
+            Rectangle square = new Rectangle(30, 30);
+            square.setFill(null);
+            square.setStroke(Color.BLACK);
+            vbox.getChildren().add(square);
+        }
+        getChildren().add(vbox);*/
     }
 
-    public void hit(){
+    public void hit() {
         health--;
     }
-    public boolean isAlive(){
+
+    public boolean isAlive() {
         return health > 0;
     }
-
 }
