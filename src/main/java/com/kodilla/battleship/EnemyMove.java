b946da2bd8c6ad1e65dcd6bc1c7697196c2386e4
kuba jarosz <kuba.jarooosz@gmail.com>
2022-02-19 00:12:09 +0100

    CPU move refacotring

--- src/main/java/com/kodilla/battleship/EnemyMove.java
+++ src/main/java/com/kodilla/battleship/EnemyMove.java
@@ -26,7 +26,7 @@ public class EnemyMove {
     public ArrayList<Board.Cell> createCellList(int x, int y){
         ArrayList<Board.Cell> cellList = new ArrayList<>();
 
-        if(!playerBoard.getCell(x - 1, y).wasShot && playerBoard.isValidPoint(x-1, y)){
+        if(!playerBoard.getCell(x - 1, y).wasShot && playerBoard.isValidPoint(x - 1, y)){
             cellList.add(playerBoard.getCell(x - 1, y));
         }
 
@@ -52,7 +52,7 @@ public class EnemyMove {
         enemyTurn = cell.shoot();
         System.out.println("First shot: " + cell.x + " " + cell.y);
 
-        while(enemyTurn){
+        if(enemyTurn){
             ArrayList<Board.Cell> possibleNextMoves = createCellList(cell.x, cell.y);
             if(!possibleNextMoves.isEmpty()) {
                 cell = possibleNextMoves.get(random.nextInt(possibleNextMoves.size()));
